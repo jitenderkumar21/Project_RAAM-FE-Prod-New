7c3eecb184448968824052bb5300d591fd416d4d
Sumit Kumar <sumit.kumar2@tigeranalytics.com>
2024-02-05 18:44:32 +0530

    local storage check

--- src/Components/ClassDetail.js
+++ src/Components/ClassDetail.js
@@ -62,7 +62,15 @@ const ClassDetail = (props) => {
   const storedSelections = localStorage.getItem("selectedTimeSlots");
   let initial_state = [];
   if (storedSelections) {
+    
     initial_state = JSON.parse(storedSelections);
+    for (const key in initial_state) {
+      const value = initial_state[key];
+      console.log(typeof(value), value)
+      if (typeof(value) !== "object"){
+        localStorage.clear()
+      } 
+    }
   } else {
     initial_state = transformedClasses;
   }
