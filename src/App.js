db151bc150e4e71ee6a6d7665f79b1d838af100e
Ramendra <ramendraverma8@gmail.com>
2024-03-23 18:05:06 +0530

    Update CmsLoginPage

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import { BrowserRouter as Router,Routes } from 'react-router-dom';
-import { Route } from 'react-router-dom';
-import './App.css';
 
+import { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import LoginPage from './Components/Admin/LoginPage';
 import MainPage from './Components/MainPage';
 import CmsReport from './Components/Admin/CmsReport';
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    const loggedIn = localStorage.getItem('isLoggedIn');
+    if (loggedIn === 'true') {
+      setIsLoggedIn(true);
+    }
+  }, []);
+  const handleLogin= () => {
+    localStorage.setItem('isLoggedIn', 'true');
+    setIsLoggedIn(true);
+  };
   return (
     <Router>
       <Routes>
-        <Route exact path="/" element={<MainPage />} />
-        <Route exact path="/admin/report" element={<CmsReport />} />
+        <Route path="/" element={<MainPage />} />
+        <Route path='/admin' element={isLoggedIn ? <CmsReport /> : <LoginPage login={handleLogin} />} />
       </Routes>
     </Router>
     
---
+++ src/Components/Admin/LoginPage.js
@@ -0,0 +1,46 @@
+// LoginPage.js
+
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import './LoginPage.css'; // Import the CSS file
+
+const LoginPage = ({ login }) => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const navigate = useNavigate();
+
+  const handleClick = () => {
+    // Navigate to the home page
+    navigate('/');
+  };
+  
+    const handleLogin = (e) => {
+      e.preventDefault();
+      if (username === 'admin' && password === 'admin') {
+        localStorage.setItem('isLoggedIn', 'true');
+        login();
+      } else {
+        setError('Invalid username or password');
+      }
+    };
+    return (
+        <div className="login-container">
+          <div className="overlay"></div>
+          <div className="login-card">
+            <h2>Login</h2>
+            <form onSubmit={handleLogin}>
+              <input type="text" placeholder="Username" className="login-input" value={username} onChange={(e) => setUsername(e.target.value)} />
+              <input type="password" placeholder="Password" className="login-input" value={password} onChange={(e) => setPassword(e.target.value)} />
+              <div className="btn-submit">
+                <button type="submit" >Login</button>
+              </div>
+              
+            </form>
+            {error && <p className="error-text">{error}</p>}
+            <div className="navigate-link"> <a onClick={handleClick} href="" >Go to Home Page!</a></div>
+      </div>
+    </div>
+  );
+    };
+export default LoginPage;
