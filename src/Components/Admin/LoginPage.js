779723f90693bb6eff139889b9c7d7f3c26ebd04
Ramendra <ramendraverma8@gmail.com>
2024-03-30 17:18:23 +0530

    Refactor LoginPage component and add environment variables

--- src/Components/Admin/LoginPage.js
+++ src/Components/Admin/LoginPage.js
@@ -1,24 +1,27 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import './LoginPage.css'; // Import the CSS file
+import "./LoginPage.css"; // Import the CSS file
 
 const LoginPage = ({ login }) => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState('');
-    const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
 
+  const user_name = process.env.REACT_APP_USERNAME;
+  const Password = process.env.REACT_APP_PASSWORD;
+  console.log(user_name)
   const handleClick = () => {
     // Navigate to the home page
-    navigate('/');
+    navigate("/");
   };
   const handleLogin = (e) => {
     e.preventDefault();
-    if (username === 'admin' && password === 'admin') {
-      localStorage.setItem('isLoggedIn', 'true');
+    if (username === user_name && password === Password) {
+      localStorage.setItem("isLoggedIn", "true");
       login();
     } else {
-      setError('Invalid username or password');
+      setError("Invalid username or password");
     }
   };
 
@@ -28,17 +31,33 @@ const LoginPage = ({ login }) => {
       <div className="login-card">
         <h2>Login</h2>
         <form onSubmit={handleLogin}>
-          <input type="text" placeholder="Username" className="login-input" value={username} onChange={(e) => setUsername(e.target.value)} />
-          <input type="password" placeholder="Password" className="login-input" value={password} onChange={(e) => setPassword(e.target.value)} />
+          <input
+            type="text"
+            placeholder="Username"
+            className="login-input"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            className="login-input"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
           <div className="btn-submit">
-                <button type="submit" >Login</button>
-              </div>
-              
-            </form>
-            {error && <p className="error-text">{error}</p>}
-            <div className="navigate-link"> <a onClick={handleClick} href="" >Go to Home Page!</a></div>
+            <button type="submit">Login</button>
+          </div>
+        </form>
+        {error && <p className="error-text">{error}</p>}
+        <div className="navigate-link">
+          {" "}
+          <a onClick={handleClick} href="">
+            Go to Home Page!
+          </a>
+        </div>
       </div>
     </div>
   );
-    };
+};
 export default LoginPage;
